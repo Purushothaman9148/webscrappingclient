9764f33bbf37736d6759771c201f9848094ffe7f
Purushothaman <purushothaman9148@gmail.com>
2022-11-13 12:34:07 +0530

    App project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
+
+const API = "http://localhost:4000/";
+
 function App() {
+  const [result, setResult] = useState([]);
+  useEffect(() => {
+    fetch(API)
+      .then(data => data.json())
+      .then(res => setResult(res))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder="search..."></input>
+      <div className="App">
+        {result.map(res => (
+          <Scrapper key={res._id} data={res} />))};
+      </div>
     </div>
   );
 }
 
+function Scrapper({ data }) {
+  return (<div>
+    <h1>{data.type}</h1>
+    <img src={data.img} alt={data.title} />
+    <p>{data.title}</p>
+    <p>{data.price}</p>
+  </div>);
+}
 export default App;
+
+
+
